5a4913693c2f04905111cce2589ff0f0a50ea214
v-techie-trader <v.techie-trader@gmail.com>
2022-01-04 20:17:41 +0530

    price handler

--- bot.py
+++ bot.py
@@ -54,6 +54,7 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("price", price))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
